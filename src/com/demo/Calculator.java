06ff8e1180ecb62e04ae224f168a724e73a81721
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-08 11:01:56 +0530

    Added : Code for subtract two number

--- src/com/demo/Calculator.java
+++ src/com/demo/Calculator.java
@@ -3,10 +3,17 @@ package com.demo;
 public class Calculator {
 
     public static void main(String[] args) {
+
         add(10,20);
+        sub(34.5,32.5);
     }
 
     public static void add(int a, int b){
-        System.out.println(a+b);
+        int sum = a + b;
+        System.out.println("Sum is : " + sum);
+    }
+    public static void sub(double a , double b){
+        double sub = a - b;
+        System.out.println("Subtract is : " + sub);
     }
 }
