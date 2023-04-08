9bde5aa18c628731e51640063e23fdd4c06d7534
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-08 10:52:43 +0530

    Added : Code for adding two number

---
+++ src/com/demo/Calculator.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        add(10,20);
+    }
+
+    public static void add(int a, int b){
+        System.out.println(a+b);
+    }
+}
